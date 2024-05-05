0c018b4b87b96d7fcabf9a56bb1f2497de1858a0
Bruthalles <thallesgmkr@gmail.com>
2024-05-04 21:46:00 -0300

    create titulo tela_login

---
+++ tela_login/src/title_login/index_title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useState} from "react"
+import { StyleSheet, View, Text, Button, TextInput, TouchableOpacity } from 'react-native';
+
+import styles from "./style_ttlogin"
+export default function TitleLog{(
+
+    <View>
+        <Text style = {styles.Txtitulo}>Login</Text>
+
+
+    </View>
+)}
---
+++ tela_login/src/title_login/style_ttlogin.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    Txtitulo: {
+        alignItems: "center",
+        fontSize:"35",
+        color: "#007FFF"
+
+
+    }
+
+
+})
