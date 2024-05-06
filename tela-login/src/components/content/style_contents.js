a14c3b6a7b202d56b9d064c2473697de05286671
Bruthalles <thallesgmkr@gmail.com>
2024-05-06 19:11:08 -0300

    reiniciando tela de login
    estilizado corretamente

---
+++ tela-login/src/components/content/style_contents.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native";
+
+import AreaLogin from "./index_content";
+
+const styles = StyleSheet.create({
+
+    texto:{
+        fontWeight:"italic",
+        fontSize:18,
+        color:"#ffd700",
+        paddingTop:10,
+    },
+    campoTexto:{
+        backgroundColor:"#e0e0e0",
+        borderRadius:7,
+        margin:2,
+       
+    },
+    btLogar:{
+        borderRadius:50,
+        
+        backgroundColor:"#0000FF",
+        paddingTop:7,
+        padding:14,
+        paddingBottom:10,
+        margin:10,
+        marginTop:20,
+        marginLeft:40,
+        width:90,
+        alignItems: "center",
+        
+        
+    },
+    txtButton:{
+        fontSize:19,
+        color: "#ffd700",
+        fontWeight:"bold",
+    }
+})
+
+export default styles
