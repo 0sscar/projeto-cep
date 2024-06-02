5c78a5f7fd0bba65cf5c1394b81556e55332ebea
thalles_rcb <thallesgmkr@gmail.com>
2024-06-01 21:03:22 -0300

    add text para testar tela

--- tela-login/src/pages/contatos/contatos.js
+++ tela-login/src/pages/contatos/contatos.js
@@ -1,9 +1,15 @@
-import { View, StyleSheet, text,Button } from "react-native";
+import { View, StyleSheet, Text,Button } from "react-native";
 
 export function Contatos(props){
     return(
         <View>
-                <Button title='Home' onPress={()=> props.navigation.navigate('home')}/>
+            
+            <Text style={{paddingTop:'20%',fontSize:20,margin:10}}>Estilize a tela de contatos</Text>
+            <Text style={{paddingTop:'10%',fontSize:20,margin:16}}>Área da Flatlist </Text>
+            <Text style={{paddingTop:'20%',fontSize:20,margin:16}}>Mais conteúdo</Text>
+            
+            <Button title='Home' onPress={()=> props.navigation.navigate('home')}/>
+
         </View>
     )
 }
