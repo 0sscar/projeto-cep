73e209e924c0da558c85fe3e596aca17a0fe7e84
thalles_rcb <thallesgmkr@gmail.com>
2024-06-07 19:59:41 -0300

    github adicionado e estilizado

--- tela-login/src/pages/contatos/contatos.js
+++ tela-login/src/pages/contatos/contatos.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text,Button, FlatList} from "react-native";
+import { View, StyleSheet, Text,Button, FlatList,Image, Linking, TouchableOpacity} from "react-native";
 import styles from "../../components/modal_login/style_contents";
 
 export function Contatos(props){
     const [people, setPeople] = useState([
         { name: 'David', key:'1' },
-        { name: 'Oscar', key:'2' },
-        { name: 'Thalles', key:'3' },
+        { name: 'Oscar', key:'oscargabrieled@gmail.com' },
+        { name: 'Thalles', key:'thallesgmkr@gmail.com' },
         { name: 'Brenno', key:'4' }
     ]);
 
+    const handlePress = () => {
+        Linking.openURL('https://github.com/0sscar/projeto-cep');
+      };
     return(
         <View style={styles.container}>
             
-            <Text style={{paddingTop:'20%',fontSize:20,margin:10}}>Estilize a tela de contatos</Text>
-            <Text style={{paddingTop:'10%',fontSize:20,margin:16}}>Área da Flatlist </Text>
+            <Text style={contato.titulo}>Estilize a tela de contatos</Text>
+            <Text style={contato.titulo}>Área da Flatlist </Text>
 
             <FlatList
                 data={people}
@@ -23,9 +26,66 @@ export function Contatos(props){
                 )}
             />
 
-            <Text style={{paddingTop:'20%',fontSize:20,margin:16}}>Mais conteúdo</Text>
+                <View style={contato.viewGit}>
+                    <Image 
+                    style={contato.octo}
+                    source={require('./octocat.png')}/>
+
+                       <TouchableOpacity 
+                       onPress={handlePress}
+                       style={contato.btnGit}>
+                        <Text style={contato.urlGit}>Clique aqui para ver o projeto no GitHub </Text>
+                       </TouchableOpacity>
+                    
+                </View>
+
+            
             <Button title='Home' onPress={()=> props.navigation.navigate('home')}/>
 
         </View>
     )
 }
+const contato = StyleSheet.create({
+    titulo:{
+        fontSize:20,
+        alignSelf:'center',
+        paddingTop:'12%'
+    },
+        
+    container:{
+        alignItems:'center',
+
+    },
+    btnGit:{
+        elevation:10,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:'#1f047a',
+        height:40,
+        margin:20,
+        marginTop:4,
+        borderRadius:50,
+        elevation:6,
+    },
+    viewGit:{
+        backgroundColor:'#fff',
+        elevation:8,
+        margin:26,
+        borderRadius:20,
+    },
+    octo:{
+        alignSelf:'center',
+        justifyContent:'center',
+        resizeMode:'contain',
+        height:50,
+        width:50,
+        margin:10,
+        marginTop:20,
+        
+    },
+    urlGit:{
+        color:'#00bbff',
+        textDecorationLine:'underline',
+        
+    },
+});
